efd32c9d6fd45106166bb90334ce42037a0d4701
codimiracle <codimiracle@outlook.com>
2020-02-08 08:57:50 +0000

    add address-view display address content

--- components/address-view.tsx
+++ components/address-view.tsx
@@ -8,22 +8,24 @@ export interface AddressViewState { };
 
 export default class AddressView extends React.Component<AddressViewProps, AddressViewState> {
   render() {
-    let address: Address = this.props.address || { address: '', postcode: '', receiver: { name: '', phone: '' } }
+    let address: Address = this.props.address || { id: '', address: '', postcode: '', receiver: { name: '', phone: '' } }
     return (
       <div className="address-view">
         <table>
-          <tr>
-            <td>地址：</td>
-            <td>{address.address}</td>
-            <td>邮编：</td>
-            <td>{address.postcode}</td>
-          </tr>
-          <tr>
-            <td>收件人：</td>
-            <td>{address.receiver.name}</td>
-            <td>联系电话：</td>
-            <td>{address.receiver.phone}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>地址：</td>
+              <td>{address.address}</td>
+              <td>邮编：</td>
+              <td>{address.postcode}</td>
+            </tr>
+            <tr>
+              <td>收件人：</td>
+              <td>{address.receiver.name}</td>
+              <td>联系电话：</td>
+              <td>{address.receiver.phone}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     )
