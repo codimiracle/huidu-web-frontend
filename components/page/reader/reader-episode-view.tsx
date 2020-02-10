caf1dfb388137bd1730819f7b0bab112eca743bb
codimiracle <codimiracle@outlook.com>
2020-02-10 15:46:19 +0000

    polish reader components for creating notes of interaction

--- components/page/reader/reader-episode-view.tsx
+++ components/page/reader/reader-episode-view.tsx
@@ -1,22 +1,110 @@
 import React from 'react';
 import { Theme } from '../../../types/theme';
 import { Episode } from '../../../types/episode';
+import Dommark from '../../../util/dommark-util';
+import { Tooltip, Form, Button } from 'antd';
+import CreateNotesDialog, { CreateNotesDialogProps } from '../../dialogs/create-notes-dialog';
+import { BookNotes } from '../../../types/notes';
+import DommarkUtil from '../../../util/dommark-util';
+
+const WrappedCreateNotesDialog = Form.create<CreateNotesDialogProps>({ name: 'create-notes-form' })(CreateNotesDialog);
 
 export interface ReaderEpisodeViewProps {
-  episode: Episode
+  episode: Episode,
+  bookNotes: BookNotes,
   theme: Theme
 }
 
 export interface ReaderEpisodeViewState {
-
+  markLeft: number,
+  markTop: number,
+  markHeight: number,
+  markRange: Range,
+  markVisible: boolean,
+  dommark: Dommark,
+  createNotesVisible: boolean,
 }
 
 export default class ReaderEpisodeView extends React.Component<ReaderEpisodeViewProps, ReaderEpisodeViewState> {
+  private pageBodyRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props: ReaderEpisodeViewProps) {
+    super(props);
+    this.state = {
+      markLeft: 0,
+      markTop: 0,
+      markHeight: 0,
+      markRange: null,
+      markVisible: false,
+      dommark: null,
+      createNotesVisible: false,
+    }
+    this.pageBodyRef = React.createRef();
+    this.onSelectionChange = this.onSelectionChange.bind(this);
+  }
+  onSelectionChange() {
+    let container = this.pageBodyRef.current;
+    let range = this.getRange();
+    if (range) {
+      let containerRect = container.getBoundingClientRect();
+      let rangeRect = range.getBoundingClientRect();
+      let left = rangeRect.left - containerRect.left + (rangeRect.width / 2);
+      let top = rangeRect.top - containerRect.top;
+      let dommark = DommarkUtil.getDommark(range, container);
+      this.setState({ markLeft: left, markTop: top, markHeight: rangeRect.height, markVisible: true, markRange: range, dommark: dommark });
+    } else {
+      this.setState({ markVisible: false });
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('selectionchange', this.onSelectionChange);
+    let relativeNotes = this.props.bookNotes.notes.filter((notes) => notes.episodeId == this.props.episode.id);
+    console.log(relativeNotes);
+    relativeNotes.map((notes) => {
+      let container = this.pageBodyRef.current;
+      let range = DommarkUtil.getRange(notes.domMark, container);
+      if (range) {
+        getSelection().addRange(range);
+      }
+    })
+  }
+  componentWillUnmount() {
+    document.removeEventListener(this.onSelectionChange);
+  }
+  private getRange() {
+    let selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+      let range = selection.getRangeAt(0);
+      return range.toString().length == 0 ? null : range
+    }
+    return null;
+  }
+  renderMarker() {
+    return (
+      <div className="marker-actions">
+        <span className="action">
+          <Button type="link" onClick={() => this.setState({ createNotesVisible: true })} style={{color: 'inherit'}}>笔记</Button>
+        </span>
+        <style jsx>{`
+          .action {
+            color: white;
+          }
+          .action:active {
+            color: #9f9e9e;
+          }
+        `}</style>
+      </div>
+    );
+  }
   render() {
     const { episode, theme } = this.props;
+    const { markLeft, markTop, markVisible, markHeight, markRange, dommark, createNotesVisible } = this.state;
     return (
       <>
-        <div className="page-body">
+        <div id={`episode-${episode.id}`} ref={this.pageBodyRef} className="page-body">
+          <Tooltip getPopupContainer={() => this.pageBodyRef.current} visible={markVisible} overlay={this.renderMarker()} placement="top">
+            <span style={{ position: 'absolute', left: `${markLeft}px`, top: `${markTop}px`, height: `${markHeight}px` }}></span>
+          </Tooltip>
           <div className="page-header">
             <h1>{episode.title || ''}</h1>
           </div>
@@ -25,18 +113,25 @@ export default class ReaderEpisodeView extends React.Component<ReaderEpisodeView
             {/* {episode.book.episodes / episode.number} */}
           </div>
         </div>
+        <WrappedCreateNotesDialog
+          episode={episode}
+          range={markRange}
+          dommark={dommark}
+          visible={createNotesVisible}
+          onCancel={() => this.setState({ createNotesVisible: false })}
+        />
         <style jsx>{`
           .page-header {
-            padding-bottom: 12px;
+            padding-bottom: 1em;
             border-bottom: 1px solid lightgrey;
           }
           .page-footer {
             height: 42px;
             position: absolute;
             bottom: 0;
-            left: 32px;
-            right: 32px;
-            padding-top: 12px;
+            left: 1.5em;
+            right: 1.5em;
+            padding-top: 1em;
             border-top: 1px solid lightgrey;
           }
           .page-content {
@@ -46,8 +141,8 @@ export default class ReaderEpisodeView extends React.Component<ReaderEpisodeView
             position: relative;
             width: 744px;
             min-height: 1056px;
-            margin: 16px auto;
-            padding: 12px 32px;
+            margin: 1.5em auto;
+            padding: 1em 1.5em;
             box-shadow: 0 4px 12px 2px darkgrey;
           }
           .page-body h1 {
@@ -69,11 +164,12 @@ export default class ReaderEpisodeView extends React.Component<ReaderEpisodeView
           }
         `}</style>
         <style jsx global>{`
-          .page-content *::selection {
+          .page-body *::selection {
             background-color: #009955!important;
           }
         `}</style>
       </>
     )
   }
+
 }
--- components/page/reader/reader-notes-view.tsx
+++ components/page/reader/reader-notes-view.tsx
@@ -1,14 +1,7 @@
-import { Drawer, List, message, Button, Divider, Popover, Form, Modal } from 'antd';
-import React, { RefObject } from 'react';
-import { API } from '../../../configs/api-config';
-import LoadingView from '../../loading-view';
-import { fetchDataByGet, fetchMessageByPost } from '../../../util/network-util';
-import { BookNotesJSON } from '../../../pages/api/user/book-notes/[book_id]';
-import { BookNotes, Note } from '../../../types/notes';
+import { Drawer, List } from 'antd';
+import React from 'react';
 import { Episode } from '../../../types/episode';
-import TextArea from 'antd/lib/input/TextArea';
-import { WrappedFormUtils } from 'antd/lib/form/Form';
-import FormItem from 'antd/lib/form/FormItem';
+import { BookNotes, Note } from '../../../types/notes';
 
 interface NoteViewPorps {
   note: Note,
@@ -36,172 +29,24 @@ function NoteView(props: NoteViewPorps) {
   )
 }
 
-export interface CreateNotesDialogProps {
-  visible: boolean,
-  form: WrappedFormUtils,
-  episode: Episode,
-  range: Range,
-  onCancel: () => void
-}
-export interface CreateNotesDialogState {
-  creating: boolean,
-}
-
-export class CreateNotesDialog extends React.Component<CreateNotesDialogProps, CreateNotesDialogState> {
-  constructor(props: CreateNotesDialogProps) {
-    super(props);
-    this.state = {
-      creating: false
-    }
-  }
-  createNotes(range: Range, notes: string) {
-    const { episode, form, onCancel } = this.props;
-    this.setState({ creating: true });
-    fetchMessageByPost(API.UserBookNotesCreate, {
-      book_id: episode.book.id,
-      episodeId: episode.id,
-      ref: range.toString(),
-      content: {
-        type: 'plaintext',
-        source: notes,
-      },
-      domMark: {
-        startDom: '',
-        startOffset: range.startOffset,
-        endDom: '',
-        endOffset: range.endOffset,
-      }
-    }).then((msg) => {
-      if (msg.code == 200) {
-        message.success('添加笔记成功!');
-        form.resetFields();
-        onCancel();
-      }
-    }).finally(() => {
-      this.setState({ creating: false });
-    })
-  }
-
-  onAddNotes() {
-    const { form, range } = this.props;
-    form.validateFields((errors) => {
-      if (!errors) {
-        this.createNotes(range, form.getFieldValue('notes'));
-      }
-    })
-  }
-  render() {
-    const { visible, form, onCancel } = this.props;
-    const { creating } = this.state;
-    return (
-      <div>
-        <Modal
-          visible={visible}
-          onCancel={onCancel}
-          title="添加笔记"
-          okText="添加"
-          onOk={() => this.onAddNotes()}
-          confirmLoading={creating}
-        >
-          <Form>
-            <FormItem>
-              {
-                form.getFieldDecorator('notes', {
-                  rules: [
-                    { required: true, message: '请输入笔记内容！' }
-                  ]
-                })(<TextArea placeholder="笔记内容"></TextArea>)
-              }
-            </FormItem>
-          </Form>
-        </Modal>
-      </div>
-    )
-  }
-}
-
-const WrappedCreateNotesDialog = Form.create<CreateNotesDialogProps>({ name: 'create-notes-form' })(CreateNotesDialog);
-
 export interface ReaderNotesViewProps {
   episode: Episode,
+  bookNotes: BookNotes,
   visible: boolean,
   onClose: () => void,
-  onNeedShow: () => void
 };
 export interface ReaderNotesViewState {
-  loading: boolean,
-  bookNotes: BookNotes,
-  selection: Selection,
-  selectedRange: Range,
-  inContainer: boolean,
-  createNotesDialogVisible: boolean
 };
 
 export default class ReaderNotesView extends React.Component<ReaderNotesViewProps, ReaderNotesViewState> {
   constructor(props: ReaderNotesViewProps) {
     super(props);
     this.state = {
-      loading: false,
-      bookNotes: null,
-      selection: null,
-      selectedRange: null,
-      inContainer: false,
-      createNotesDialogVisible: false
     }
   }
-  fetchNotesList() {
-    const { episode } = this.props;
-    this.setState({ loading: true });
-    fetchDataByGet<BookNotesJSON>(API.UserBookNotesEntity, {
-      book_id: episode.book.id
-    }).then((data) => {
-      this.setState({ bookNotes: data.bookNotes });
-    }).catch((err) => {
-      console.log(err);
-      message.error(`获取笔记数据失败：${err}`);
-    }).finally(() => {
-      this.setState({ loading: false });
-    });
-  }
-  componentDidMount() {
-    let container = document.querySelector('.episode-list');
-    container.addEventListener('mouseleave', () => {
-      this.setState({ inContainer: false });
-    })
-    container.addEventListener('mousemove', () => {
-      this.setState({ inContainer: true });
-    })
-    container.addEventListener('mouseup', () => {
-      if (this.state.selection) {
-        this.props.onNeedShow();
-      }
-    })
-    document.addEventListener('selectionchange', () => {
-      if (!this.state.inContainer) {
-        return;
-      }
-      let selection = window.getSelection();
-      if (selection.rangeCount == 0) {
-        selection = null;
-      } else {
-        if (selection.getRangeAt(0).toString().length == 0) {
-          selection = null;
-        }
-      }
-      this.setState({ selection: selection });
-    });
-    this.fetchNotesList();
-  }
   render() {
-    const { visible, episode, onClose } = this.props;
-    const { loading, bookNotes, selection, selectedRange, createNotesDialogVisible } = this.state;
+    const { visible, episode, onClose, bookNotes } = this.props;
     let notes = bookNotes && bookNotes.notes || [];
-    let onCreateNotesDialogCancel = () => {
-      this.setState({ createNotesDialogVisible: false, selection: null });
-      // fix dialog close bug.
-      document.body.style = '';
-      document.body.className = '';
-    };
     return (
       <>
         <Drawer
@@ -213,37 +58,14 @@ export default class ReaderNotesView extends React.Component<ReaderNotesViewProp
           width="312px"
           onClose={onClose}
         >
-          <div>
-            {
-              selection &&
-              <div>
-                <p>选中文本：</p>
-                <p>{selection.getRangeAt(0).toString()}</p>
-                <div style={{ textAlign: 'center', padding: '8px' }}>
-                  <Button onClick={() => this.setState({ selectedRange: selection.getRangeAt(0).cloneRange(), createNotesDialogVisible: true })}>添加笔记</Button>
-                </div>
-                <Divider type="horizontal" />
-              </div>
-            }
-          </div>
-          <LoadingView loading={loading}>
-            <List
-              renderItem={(note) => (
-                <List.Item>
-                  <NoteView note={note} />
-                </List.Item>
-              )}
-              dataSource={notes}
-            />
-          </LoadingView>
-          <div>
-            <WrappedCreateNotesDialog
-              visible={createNotesDialogVisible}
-              episode={episode}
-              range={selectedRange}
-              onCancel={onCreateNotesDialogCancel}
-            />
-          </div>
+          <List
+            renderItem={(note) => (
+              <List.Item>
+                <NoteView note={note} />
+              </List.Item>
+            )}
+            dataSource={notes}
+          />
         </Drawer>
       </>
     )
