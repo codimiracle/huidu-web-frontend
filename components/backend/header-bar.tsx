18d0b0780f1b61c7e1f560a3a46b70b221c4d0be
codimiracle <codimiracle@outlook.com>
2020-02-18 08:13:55 +0000

    polish backend header bar

--- components/backend/header-bar.tsx
+++ components/backend/header-bar.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Divider } from 'antd';
 
 export interface HeaderBarProps {
   title: string,
-  hint: string
+  hint: string,
+  extra?: ReactNode
 };
 export interface HeaderBarState { };
 
@@ -11,9 +12,22 @@ export default class HeaderBar extends React.Component<HeaderBarProps, HeaderBar
   render() {
     return (
       <>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.hint}</p>
+        <div className="backend-header-bar">
+          <div className="backend-header-title">
+            <h2>{this.props.title}</h2>
+            <p>{this.props.hint}</p>
+          </div>
+          <div className="backend-header-extra">
+            {this.props.extra}
+          </div>
+        </div>
         <Divider type="horizontal" />
+        <style jsx>{`
+          .backend-header-bar {
+            display: flex;
+            justify-content: space-between;
+          }
+        `}</style>
       </>
     )
   }
