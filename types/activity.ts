f40d8b1502b742e212b45f1638a2f99256bff75b
codimiracle <codimiracle@outlook.com>
2020-02-21 08:54:32 +0000

    polish activity type definition

--- types/activity.ts
+++ types/activity.ts
@@ -1,9 +1,25 @@
 import { Commodity } from "./commodity";
 import { Book } from "./book";
 
+export enum ActivityStatus {
+  Deactivated = 'deactivated',
+  Activated = 'activated',
+}
+
+export const ACTIVITY_STATUS_TEXTS = {};
+ACTIVITY_STATUS_TEXTS[ActivityStatus.Activated] = "激活";
+ACTIVITY_STATUS_TEXTS[ActivityStatus.Deactivated] = "未激活";
+
+export const ACTIVITY_STATUS_COLORS = {};
+ACTIVITY_STATUS_COLORS[ActivityStatus.Activated] = "geekblue";
+ACTIVITY_STATUS_COLORS[ActivityStatus.Deactivated] = "red";
+
 export interface Activity {
   id: string,
   banner: string,
   url: string,
-  book: Book
+  status: ActivityStatus,
+  book: Book,
+  createTime: string,
+  updateTime: string
 }
