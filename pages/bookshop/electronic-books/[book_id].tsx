a163871e9ca81191bf2ce933f38b104aac454565
codimiracle <codimiracle@outlook.com>
2020-04-09 20:37:04 +0000

    add burying point for book details

--- pages/bookshop/electronic-books/[book_id].tsx
+++ pages/bookshop/electronic-books/[book_id].tsx
@@ -12,7 +12,7 @@ import { EntityJSON } from '../../../types/api';
 import { Catalogs, ElectronicBook } from '../../../types/electronic-book';
 import { Episode } from '../../../types/episode';
 import DatetimeUtil from '../../../util/datetime-util';
-import { fetchDataByGet } from '../../../util/network-util';
+import { fetchDataByGet, fetchMessageByPost, fetchMessageByGet } from '../../../util/network-util';
 
 const { TabPane } = Tabs;
 
@@ -46,6 +46,12 @@ export default class BookDetails extends React.Component<BookDetailsProps, BookD
     super(props);
     this.state = {
     }
+  }
+  componentDidMount() {
+    fetchMessageByGet(API.ElectronicBookEntity, {
+      details: true,
+      book_id: this.props.book.id
+    });
   }
   render() {
     const { book, lastUpdate } = this.props;
@@ -86,12 +92,12 @@ export default class BookDetails extends React.Component<BookDetailsProps, BookD
             <div className="marks-details">
               <div>
                 <div>评论：<Rate disabled allowHalf defaultValue={book.rate} style={{ fontSize: '18px' }} /></div>
-                <div>点评：<Rate disabled allowHalf defaultValue={5} style={{ fontSize: '18px' }} /></div>
+                <div>点评：<Rate disabled allowHalf defaultValue={book.reviewRate} style={{ fontSize: '18px' }} /></div>
               </div>
               <div>
                 <div className="comprehensive-rate">
                   <div><strong>综合</strong></div>
-                  <div className="comprehensive-mark"><strong>{book.rate}</strong></div>
+                  <div className="comprehensive-mark"><strong>{book.avgRate}</strong></div>
                 </div>
                 <div className="mark-unit">分</div>
               </div>
@@ -113,7 +119,7 @@ export default class BookDetails extends React.Component<BookDetailsProps, BookD
               {
                 book.category && book.category.tags && book.category.tags.length ?
                   book.category.tags.map((tag: any) =>
-                    <Link href={`/categories/[category_id]?tag=${tag.name}`} as={`/categories/${book.category.id}?tag=${tag.name}`}><a>{tag.name}</a></Link>
+                    <Link key={tag.id} href={`/categories/[category_id]?tag=${tag.name}`} as={`/categories/${book.category.id}?tag=${tag.name}`}><a>{tag.name}</a></Link>
                   )
                   : <span>无标签</span>
               }
